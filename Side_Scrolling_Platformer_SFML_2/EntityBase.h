692b5690f2e1330c0b296da2aacd1c2e290a451c
Tung <Fizzy@users.noreply.github.com>
2020-04-09 16:14:50 +0700

    Thêm EntityBase và EntityManager

---
+++ Side_Scrolling_Platformer_SFML_2/EntityBase.h
@@ -0,0 +1,65 @@
+#pragma once
+#include"Map.h"
+
+struct CollisionElement
+{
+	CollisionElement(float l_area,TileInfo * l_CollisonTile,const sf::FloatRect & l_tileBounds ) :
+	m_area(l_area), m_CollisonTile(l_CollisonTile), m_tileBounds(l_tileBounds) {}
+	sf::FloatRect m_tileBounds;
+	float m_area;
+	TileInfo * m_CollisonTile;	
+};
+using Collisions = std::vector<CollisionElement>;
+
+enum class EntityType {Base, Enemy, Player}
+enum class EntityState {
+	Idle, Walking, Jumping, Attacking, Hurt, Dying
+}
+class EntityManager;
+class EntityBase{
+	friend class EntityManager;
+public:
+	EntityBase(EntityManager * l_entityMgr);
+	virtual ~EntityBase();
+	//Getter and setter
+
+	//other function
+	void Move(float l_x, float l_y);
+	void AddVelocity(float l_x, float l_y);
+	void Accelerate(float l_x, float l_y);
+	void SetAcceleration(float l_x, float l_y);
+	void ApplyFriction(float l_x, float l_y);
+	virtual void Update(float l_dt);
+	virtual void Draw(sf::RenderWindow * l_window )=0;
+protected:
+	//Methods
+	void UpdateAABB();
+	void CheckCollision();
+	void ResolveCollision();
+
+	//Method for what THIS entity does TO the l_collider entity
+	virtual void OnEntityCollision(EntityBase * l_collider, bool l_attack) =0;
+
+protected:
+	std::string m_name;
+	EntityType m_type;
+	unsigned int m_id; //Entity id in the entity manager
+	sf::Vector2f m_position; // Current position
+	sf::Vector2f m_positionOld; //Position before entity moved
+	sf::Vector2f m_velocity; // Current velocity
+	sf::Vector2f m_maxVelocity; // Maximum velocity
+	sf::Vector2f m_speed; // Value of acceleration
+	sf::Vector2f m_acceleration; // Current acceleration
+	sf::Vector2f m_friction; // Friction value
+	TileInfo * m_referenceTile; //Tile underneath entity
+	sf::Vector2f m_size; // Size of collision box
+	sf::FloatRect m_AABB; // The bounding box for collision
+	EntityState m_currentState; // Current entity state
+
+	//Flags for remembering axis collisions
+	bool m_collidingX;
+	bool m_collidingY;
+
+	Collisions m_collisions;
+	EntityManager * m_entityMgr;
+};
---
+++ Side_Scrolling_Platformer_SFML_2/EntityManager.h
@@ -0,0 +1,47 @@
+#include"SharedContext.h"
+#include"EntityBase.h"
+
+using EntityContainer = std::unordered_map<unsigned int, Entity Base *>;
+using EntityFactory = std::unordered_map<EntityType, std::function<EntityBase*(void)>>; 
+using EnemyTypes = std::unordered_map<std::string,std::string>;
+class EntityManager
+{
+public:
+	EntityManager(SharedContext * l_context, unsigned int l_maxEntity);
+	~EntityManager();
+
+	void Add(const EntityType & l_type, const std::string & l_name = "");
+	EntityBase * Find(unsigned int l_id);
+	EntityBase * Fine(const std::string & l_name)
+	void Remove(unsigned int l_id);
+
+	void Update(float l_dt);
+	void Draw();
+
+	void Purge();
+
+	SharedContext * GetContext();
+private: //Methods
+	template<class T>
+	void RegisterEntity(const EntityType * l_type)
+	{
+		m_entityFactory[l_type] = [this] () -> EntityBase *
+		{
+			return new T(this);
+		}
+	}
+
+	void ProcessRemovals();
+	void LoadEnemyType(const std::string & l_name);
+	void EntityCollisionCheck();
+
+private: //Datas
+	EntityContainer m_entities;
+	EnemyTypes m_enemyTypes;
+	EntityFactory m_entityFactory;
+	SharedContext * m_context;
+	unsigned int m_counter;
+	unsigned int m_maxEntity;
+
+	std::vector<unsigned int> m_entitiesToRemove;
+};
